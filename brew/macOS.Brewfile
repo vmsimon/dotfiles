dfbe8ad4ce73536217fc8781efef98f487a20834
vmsimon <vmsimon@oev.de>
2023-07-20 14:56:10 +0200

    modifications

--- brew/macOS.Brewfile
+++ brew/macOS.Brewfile
@@ -1,71 +1,67 @@
+# See: https://github.com/Homebrew/homebrew-bundle
+
 ##################### taps #####################
-# tap "heroku/brew"  #  to create and manage your Heroku apps
-tap "homebrew/bundle"
+# needed tap's
 tap "homebrew/cask"
 tap "homebrew/cask-fonts"
 tap "homebrew/core"
 tap "homebrew/services" # Integrates Homebrew formulae with macOS' launchctl manager.
+tap "1password/tap" # 1Password-CLI - https://developer.1password.com/docs/cli/shell-plugins/homebrew/
+tap "derailed/k9s" # Kubernetes CLI To Manage Your Clusters In Style!
+tap "derailed/popeye" # Popeye - A Kubernetes Cluster Sanitizer
+
 
 ##################### brew #####################
-brew "ack" # ack 2.14 is a tool like grep, optimized for programmers
-brew "cmake" # for YouCompleteMe vim plugin ## the cross-platform, open-source build system
 brew "coreutils" # for awesome commands like timeout
-brew "extract_url" # for urlview tmux plugin
-brew "ffmpeg" # for being able to manipulate videos for example
-brew "fish" # a smart and user-friendly command line shel
-brew "gifsicle" # for gif_compress fish function ## a command-line tool for creating, editing, and getting information about GIF images and animations
+brew "zsh" # UNIX shell (command interpreter)
 brew "git" # a free and open source distributed version control system
-brew "python" # a programming language that lets you work quickly and integrate systems more effectively
-brew "python@2" # Version 2 of python
+
 brew "httpie" # CLI HTTP client
-# brew "hub" # a command-line wrapper for git that makes you better at GitHub
-brew "m-cli" # Swiss Army Knife for macOS
-# brew "mariadb", restart_service: :changed # a drop-in replacement for MySQ
-brew "mas" # A simple command line interface for the Mac App Store
-brew "node" # a JavaScript runtime built on Chrome's V8 JavaScript engine
-brew "p7zip" # a port of 7za.exe for MacOS X
-brew "pipenv" #  a tool that aims to bring the best of all packaging worlds (bundler, composer, npm, cargo, yarn, etc.) to the Python world
-# brew "postgresql", restart_service: :changed # a open source database
-brew "reattach-to-user-namespace" # for tmux commands to work
-# brew "redis", restart_service: :changed # an open source (BSD licensed), in-memory data structure store
-brew "tldr" # Simplified and community-driven man pages
-brew "tmux" #a terminal multiplexer
+#brew "m-cli" # Swiss Army Knife for macOS
+
 brew "trash" # a small command-line program for OS X that moves files or folders to the trash
 brew "tree" # a recursive directory listing command
 brew "vim" # the ubiquitous text editor
 brew "wget" # a free software package for retrieving files
-brew "wifi-password" # CLI for Get the password of the wifi you're on, from the Keychain
-brew "yarn" # FAST, RELIABLE, AND SECURE DEPENDENCY MANAGEMENT
-brew "youtube-dl" # to download videos from YouTube.com and a few more sites
-brew "z" # jump aroound - Tracks your most used directories, based on 'frecency'.
-#brew "heroku/brew/heroku" # The Heroku Command Line Interface (CLI) makes it easy to create and manage your Heroku apps directly from the terminal. It’s an essential part of using Heroku.
+
+
+#brew "wifi-password" # CLI for Get the password of the wifi you're on, from the Keychain
+#brew "z" # jump aroound - Tracks your most used directories, based on 'frecency'.
+
+brew "watch"
+brew "k9s"
+brew "popeye"
+brew "pwgen"
+brew "htop"
+
+brew "kubernetes-cli"
+brew "helm"
+brew "helmfile"
+brew "stern"
+brew "s5cmd"
+
+# set arguments for all 'brew install --cask' commands
+cask_args appdir: "~/Applications", require_sha: true
 
 ##################### cask #####################
-#cask "1password"
-#cask "alfred"
-#cask "bartender" # lets you organize your menu bar icons
-cask "docker"
-#cask "dropbox"
 cask "firefox"
-cask "font-fira-mono-for-powerline" # for iTerm2 powerline
-cask "gimp"
 cask "google-chrome"
-# can be removed if media keys started working like they should again
-cask "highsierramediakeyenabler" # macOS High Sierra Media Key Enabler for iTunes and Spotify - If you want even more control over what you want to control you should try beardedspice
 cask "iterm2"
-cask "microsoft-office"
-#cask "postman"
-#cask "robo-3t" # free lightweight GUI for MongoDB
-#cask "sequel-pro" # MySQL/MariaDB database management for macOS
-cask "spectacle" # Move and resize windows on MacOS X with ease
-# cask "spotify" # Music and Videostreaming
-cask "atom" # the Atom Editor
-cask "teamviewer" # secure remote desktop access and support
-cask "the-unarchiver" # Open any archive in seconds
-cask "tunnelblick" #  a free, open source graphic user interface for OpenVPN
-cask "vlc" # a free and open source cross-platform multimedia player
-#cask "zoomus" # video communications
+cask "1password"
+cask "1password-cli"
+cask "wireshark"
+cask "xca"
+cask "textmate"
+cask "font-fira-mono-for-powerline" # for iTerm2 powerline
+cask "postman"
+cask "clipy"
+cask "rectangle" # Move and resize windows on MacOS X with ease
+cask "browserosaurus"
+cask "lens"
+cask "slack"
+cask "apache-directory-studio"
+cask "mysqlworkbench"
 
 ##################### mas #####################
-mas "Amphetamine", id: 937984704 # effortlessly override your energy saver settings and keep your Mac awake
-mas "GIPHY CAPTURE", id: 668208984 # to create GIFs on your Mac
+
+
---
+++ macos_bootstrap.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# inspired by
+# https://gist.github.com/codeinthehole/26b37efa67041e1307db
+# https://github.com/why-jay/osx-init/blob/master/install.sh
+# https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
+
+# PRECONDITIONS
+# 1)
+# make sure the file is executable
+# chmod +x macos_bootstrap.sh
+#
+# 2)
+# Your password may be necessary for some packages
+#
+
+main() {
+    info "macOS bootstrapping start"
+
+    # https://docs.brew.sh/Installation#macos-requirements
+    xcode-select --install
+    # `set -eu` causes an 'unbound variable' error in case SUDO_USER is not set
+    SUDO_USER=$(whoami)
+
+    # First things first, asking for sudo credentials
+    ask_for_sudo
+    # Installing Homebrew, the basis of anything and everything
+    install_homebrew
+    # Installing mas using brew as the requirement for login_to_app_store
+    brew_install mas
+    # Ensuring the user is logged in the App Store so that
+    # install_packages_with_brewfile can install App Store applications
+    # using mas cli application
+    login_to_app_store
+
+    # Cloning Dotfiles repository for install_packages_with_brewfile
+    # to have access to Brewfile
+    #clone_dotfiles_repo
+    # Installing all packages in Dotfiles repository's Brewfile
+    install_packages_with_brewfile
+    # Changing default shell to Fish
+    change_shell_to_fish
+    # Configuring git config file
+    configure_git
+
+    # Installing powerline-status so that setup_symlinks can setup the symlinks
+    # and requests and dotenv as the basis for a regular python script
+    #pip_packages=(powerline-status requests python-dotenv flake8)
+    #pip3_install "${pip_packages[@]}"
+    # Installing typescript so that YouCompleteMe can support it
+    # and prettier so that Neoformat can auto-format files
+    #yarn_packages=(prettier typescript)
+    #yarn_install "${yarn_packages[@]}"
+    # Setting up symlinks so that setup_vim can install all plugins
+    #setup_symlinks
+    # Setting up Vim
+    #setup_vim
+    # Setting up tmux
+    #setup_tmux
+    # Configuring iTerm2
+    #configure_iterm2
+    # Update /etc/hosts
+    #update_hosts_file
+    # Setting up macOS defaults
+    #setup_macOS_defaults
+    # Updating login items
+    #update_login_items
+
+    brew_cleanup
+
+    info "macOS bootstrapping done"
+
+}
+
+function ask_for_sudo() {
+    info "Prompting for sudo password..."
+    if sudo --validate; then
+        # Keep-alive
+        while true; do sudo --non-interactive true; \
+            sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+        success "Sudo credentials updated."
+    else
+        error "Obtaining sudo credentials failed."
+        exit 1
+    fi
+}
+
+function login_to_app_store() {
+    info "Logging into app store..."
+    if mas account >/dev/null; then
+        success "Already logged in."
+    else
+        open -a "/Applications/App Store.app"
+        until (mas account > /dev/null);
+        do
+            sleep 3
+        done
+        success "Login to app store successful."
+    fi
+}
+
+function install_homebrew() {
+    info "Installing Homebrew..."
+    if hash brew 2>/dev/null; then
+        success "Homebrew already exists."
+    else
+        url=https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+        if /usr/bin/ruby -e "$(curl -fsSL ${url})"; then
+            success "Homebrew installation succeeded."
+        else
+            error "Homebrew installation failed."
+            exit 1
+        fi
+    fi
+}
+
+function install_packages_with_brewfile() {
+    info "Installing packages within ./brew/macOS.Brewfile ..."
+    if brew bundle --file=./brew/macOS.Brewfile; then
+        success "Brewfile installation succeeded."
+    else
+        error "Brewfile installation failed."
+        exit 1
+    fi
+}
+
+function brew_install() {
+    package_to_install="$1"
+    info "brew install ${package_to_install}"
+    if hash "$package_to_install" 2>/dev/null; then
+        success "${package_to_install} already exists."
+    else
+        if brew install "$package_to_install"; then
+            success "Package ${package_to_install} installation succeeded."
+        else
+            error "Package ${package_to_install} installation failed."
+            exit 1
+        fi
+    fi
+}
+
+function brew_cleanuo() {
+    info "brew cleanup"
+    if brew cleanup; then
+        success "Cleanup succeeded."
+    else
+        error "Cleanup failed."
+        exit 1
+    fi
+}
+
+function configure_git() {
+    username="Volker Meyer-Simon"
+    email="vmsimon@oev.de"
+
+    info "Configuring git..."
+    if git config --global user.name "$username" && \
+       git config --global user.email "$email"; then
+        success "git configuration succeeded."
+    else
+        error "git configuration failed."
+    fi
+}
+
+function login_item() {
+    path=$1
+    hidden=${2:-false}
+    name=$(basename "$path")
+
+    # "¬" charachter tells osascript that the line continues
+    if osascript &> /dev/null << EOM
+tell application "System Events" to make login item with properties ¬
+{name: "$name", path: "$path", hidden: "$hidden"}
+EOM
+then
+    success "Login item ${name} successfully added."
+else
+    error "Adding login item ${name} failed."
+    exit 1
+fi
+}
+
+function coloredEcho() {
+    local exp="$1";
+    local color="$2";
+    local arrow="$3";
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput bold;
+    tput setaf "$color";
+    echo "$arrow $exp";
+    tput sgr0;
+}
+
+function info() {
+    coloredEcho "$1" blue "========>"
+}
+
+function substep() {
+    coloredEcho "$1" magenta "===="
+}
+
+function success() {
+    coloredEcho "$1" green "========>"
+}
+
+function error() {
+    coloredEcho "$1" red "========>"
+}
+
+main "$@"
